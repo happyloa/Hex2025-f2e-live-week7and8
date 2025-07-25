f0624a46f2d521eaf5a182a1be264d96598eeff9
Aaron <piafyoyo06@gmail.com>
2025-07-25 21:25:22 +0800

    style: 預設樣式

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div
+    class="text-primary text-body2 md:text-body selection:bg-primary-120 selection:text-primary-10"
+  >
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
