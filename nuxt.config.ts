431dff323a47314432f05fc81f272adaf91bdf7a
Aaron <piafyoyo06@gmail.com>
2025-07-25 19:28:42 +0800

    feat: 安裝 nuxt swiper 套件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/google-fonts', '@nuxtjs/tailwindcss']
+  modules: [
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss',
+    'nuxt-swiper'
+  ]
 })
