137c5b92a8558073a7612406b48e95b0c87f0b77
Aaron <piafyoyo06@gmail.com>
2025-07-27 14:36:54 +0800

    feat: 熱門分類區塊

--- app/pages/search-result.vue
+++ app/pages/search-result.vue
@@ -1,1 +1,84 @@
-<template><h1>地點搜尋頁</h1></template>
+<template>
+  <!-- 熱門分類 -->
+  <section class="px-3 py-10 md:py-20">
+    <div class="max-w-container mx-auto">
+      <h2 class="md:text-h1 text-h3 mb-5 md:mb-10">
+        想怎麼玩大阪？熱門分類快速選！
+      </h2>
+      <ul class="grid grid-cols-2 gap-4 md:grid-cols-4 md:gap-6">
+        <!-- 分類1 -->
+        <li>
+          <article class="border-neutral-40 rounded-2xl border md:rounded-3xl">
+            <div
+              class="bg-primary-10 justify-items-center rounded-t-2xl py-5 md:rounded-t-3xl"
+            >
+              <img
+                src="/images/search-result/category-1.svg"
+                alt="交通優惠"
+                class="w-16 md:w-[150px]"
+              />
+            </div>
+            <footer class="p-4 md:p-6">
+              <h3 class="md:text-h6 text-title mb-2 text-black">交通優惠</h3>
+              <p class="text-neutral-80">20+ 種選擇</p>
+            </footer>
+          </article>
+        </li>
+        <!-- 分類2 -->
+        <li>
+          <article class="border-neutral-40 rounded-2xl border md:rounded-3xl">
+            <div
+              class="bg-primary-10 justify-items-center rounded-t-2xl py-5 md:rounded-t-3xl"
+            >
+              <img
+                src="/images/search-result/category-2.svg"
+                alt="一日遊"
+                class="w-16 md:w-[150px]"
+              />
+            </div>
+            <footer class="p-4 md:p-6">
+              <h3 class="md:text-h6 text-title mb-2 text-black">一日遊</h3>
+              <p class="text-neutral-80">50+ 種選擇</p>
+            </footer>
+          </article>
+        </li>
+        <!-- 分類3 -->
+        <li>
+          <article class="border-neutral-40 rounded-2xl border md:rounded-3xl">
+            <div
+              class="bg-primary-10 justify-items-center rounded-t-2xl py-5 md:rounded-t-3xl"
+            >
+              <img
+                src="/images/search-result/category-3.svg"
+                alt="景點門票"
+                class="w-16 md:w-[150px]"
+              />
+            </div>
+            <footer class="p-4 md:p-6">
+              <h3 class="md:text-h6 text-title mb-2 text-black">景點門票</h3>
+              <p class="text-neutral-80">70+ 種選擇</p>
+            </footer>
+          </article>
+        </li>
+        <!-- 分類4 -->
+        <li>
+          <article class="border-neutral-40 rounded-2xl border md:rounded-3xl">
+            <div
+              class="bg-primary-10 justify-items-center rounded-t-2xl py-5 md:rounded-t-3xl"
+            >
+              <img
+                src="/images/search-result/category-4.svg"
+                alt="美食餐廳"
+                class="w-16 md:w-[150px]"
+              />
+            </div>
+            <footer class="p-4 md:p-6">
+              <h3 class="md:text-h6 text-title mb-2 text-black">美食餐廳</h3>
+              <p class="text-neutral-80">80+ 種選擇</p>
+            </footer>
+          </article>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
