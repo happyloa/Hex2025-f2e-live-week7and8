ca3d0aacb705e410d73cc8612a62ef81e7ed2f9b
Aaron <piafyoyo06@gmail.com>
2025-08-01 23:35:43 +0800

    feat: 進到會員頁面時把 isLoggedIn 設為 true

--- app/layouts/member.vue
+++ app/layouts/member.vue
@@ -1,3 +1,11 @@
+<script setup>
+// 進到使用這個 layout 的頁面時，自動把 sessionStorage.isLoggedIn 設成 'true'
+// 這樣做是為了模擬已登入狀態，因為只有登入以後才可能進到會員專屬頁面（個人資訊、常用旅客）
+onMounted(() => {
+  sessionStorage.setItem("isLoggedIn", "true");
+});
+</script>
+
 <template>
   <section class="relative overflow-x-hidden bg-neutral-15 px-3">
     <!-- 裝飾線條 -->
