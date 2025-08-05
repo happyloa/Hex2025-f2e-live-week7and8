2dd9f762020930495a0c321702e52ef2c7fc4a96
Aaron <piafyoyo06@gmail.com>
2025-08-06 02:14:28 +0800

    style: 放大鏡 icon 尺寸

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -72,11 +72,16 @@ const marqueeSpeed = 20;
         <!-- 搜尋框與放大鏡 icon -->
         <div class="flex flex-1 items-center gap-2">
           <!-- 放大鏡 icon -->
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            class="shrink-0 text-neutral"
+          >
             <path
               d="m19.6 21-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.887T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5a6.1 6.1 0 0 1-1.3 3.8l6.3 6.3zM9.5 14q1.875 0 3.188-1.312Q14 11.375 14 9.5t-1.312-3.187T9.5 5 6.313 6.313 5 9.5t1.313 3.188T9.5 14"
               fill="currentColor"
-              class="text-neutral"
             />
           </svg>
           <input
